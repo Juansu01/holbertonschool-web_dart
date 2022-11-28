a4baa920651a32f1ba3bc5a1869dca3acddd5cf4
Juansu01 <thejuank0@hotmail.com>
2022-11-28 18:42:26 -0500

    Fix: Only 5 lines

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,8 +1,5 @@
-import "dart:io";
-
 void main() {
   String str = "Holberton School";
-  String subString = str.substring(0, 9);
   print(str * 3);
-  print('$subString\n');
+  print('Holberton\n');
 }
