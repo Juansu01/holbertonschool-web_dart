d6ca5ca1b4d57bd3605e068926dc626eabfa2747
Juansu01 <thejuank0@hotmail.com>
2022-11-29 15:18:44 -0500

    Reformating

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,33 +1,21 @@
 int whoWins(Map<String, int> team1, Map<String, int> team2) {
   
-  int teamAFreeThrows = 0;
-  int teamA2Pointer = 0;
-  int teamA3Pointer = 0;
+  num getScore(Map team) {
+    num points = 0;
+    team.forEach((key, value) {
+      if (key == 'Free throws') {
+        points += value;
+      } else if (key == '2 pointer') {
+        points += value * 2;
+      } else if (key == '3 pointer') {
+        points += value * 3;
+      }
+    });
+  return points;
+  }
 
-  int teamBFreeThrows = 0;
-  int teamB2Pointer = 0;
-  int teamB3Pointer = 0;
-
-  team1.forEach((key, value) => {
-    if (key == 'Free throws'){
-      teamAFreeThrows = value
-    }else if (key == '2 pointer'){
-      teamA2Pointer = value * 2
-    } else 
-      teamA3Pointer = value * 3
-  });
-
-  team2.forEach((key, value) => {
-    if (key == 'Free throws'){
-      teamBFreeThrows = value
-    }else if (key == '2 pointer'){
-      teamB2Pointer = value * 2
-    } else 
-      teamB3Pointer = value * 3
-  });
-
-  int teamAScore = teamAFreeThrows + teamA2Pointer + teamA3Pointer;
-  int teamBScore = teamBFreeThrows + teamB2Pointer + teamB3Pointer;
+  num teamAScore = getScore(team1);
+  num teamBScore = getScore(team2);
   
   return teamAScore > teamBScore ? 1 : teamAScore < teamBScore ? 2 : 0;
 }
