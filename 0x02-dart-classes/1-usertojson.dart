ab121ea75679d6a902e708985f87736dfedf3367
Juansu01 <thejuank0@hotmail.com>
2022-12-02 09:49:22 -0500

    Fix

--- 0x02-dart-classes/1-usertojson.dart
+++ 0x02-dart-classes/1-usertojson.dart
@@ -3,7 +3,11 @@ Class User {
     int age;
     double height;
 
-    User({ required this.name, required this.age, required this.height });
+    User({name: String, age: int, height: double}) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
 
     Map toJson() {
         return {'name': this.name, 'age': this.age, 'height': this.height };
