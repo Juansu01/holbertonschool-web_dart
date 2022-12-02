1e82e372a7c660ec0743b145507a503324cfa080
Juansu01 <thejuank0@hotmail.com>
2022-12-02 09:55:57 -0500

    Fix typo

--- 0x02-dart-classes/1-usertojson.dart
+++ 0x02-dart-classes/1-usertojson.dart
@@ -1,13 +1,9 @@
-Class User {
+class User {
     String name;
     int age;
     double height;
 
-    User({name: String, age: int, height: double}) {
-        this.name = name;
-        this.age = age;
-        this.height = height;
-    }
+    User({ required this.name, required this.age, required this.height });
 
     Map toJson() {
         return {'name': this.name, 'age': this.age, 'height': this.height };
