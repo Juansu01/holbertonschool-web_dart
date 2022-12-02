4bee0b73fa6ba51a375e26a9bcf8d6114d8665f6
Juansu01 <thejuank0@hotmail.com>
2022-12-02 09:47:11 -0500

    Add: Private Password

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,21 @@
+class Password {
+    String _password;
+    Password({required String password}) : _password = password;
+    String get password => _password;
+
+    bool isValid()  {
+        if (password.length >= 8 && password.length <= 16) {
+            if (this.password.contains(RegExp(r'[A-Z]')) &&
+                this.password.contains(RegExp(r'[a-z]'))) {
+                if (this.password.contains(RegExp(r'[0-9]'))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    String toString() {
+        return "Your Password is: ${this.password}";
+    }
+}
