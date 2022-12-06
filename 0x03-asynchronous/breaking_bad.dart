dfa23e42be1e4e7bce3905ca24296c222880d67d
Juansu01 <thejuank0@hotmail.com>
2022-12-06 11:05:26 -0500

    Add: Breaking Bad Characters

---
+++ 0x03-asynchronous/breaking_bad.dart
@@ -0,0 +1,17 @@
+import "package:http/http.dart" as http;
+import 'dart:convert';
+
+printBbCharacters() async {
+  var res = Uri.https("breakingbadapi.com", "/api/characters/");
+  try {
+    var response = await http.get(res);
+    if (response.statusCode == 200) {
+      List<dynamic> jsonRes = convert.jsonDecode(response.body);
+      for (int i = 0; i < jsonRes.length; i++) {
+        print(jsonRes[i]['name']);
+      }
+    }
+  } catch (error) {
+    return "error caught: $error";
+  }
+}
